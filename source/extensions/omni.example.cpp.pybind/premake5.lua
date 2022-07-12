362cc59333d28efd2c5cb9f37ff5761c59ff57f1
dbosnich <dbosnich@nvidia.com>
2022-07-12 14:26:24 -0600

    Updated some comments and formatting.

--- source/extensions/omni.example.cpp.pybind/premake5.lua
+++ source/extensions/omni.example.cpp.pybind/premake5.lua
@@ -1,8 +1,8 @@
--- Get the info associated with this extension.
+-- Setup the extension.
 local ext = get_current_extension_info()
+project_ext(ext)
 
 -- Link folders that should be packaged with the extension.
-project_ext(ext)
 repo_build.prebuild_link {
     { "data", ext.target_dir.."/data" },
     { "docs", ext.target_dir.."/docs" },
--- source/extensions/omni.example.python.ui/premake5.lua
+++ source/extensions/omni.example.python.ui/premake5.lua
@@ -1,10 +1,10 @@
--- Get the info associated with this extension.
+-- Setup the extension.
 local ext = get_current_extension_info()
+project_ext(ext)
 
 -- Link folders that should be packaged with the extension.
-project_ext(ext)
-    repo_build.prebuild_link {
-        { "data", ext.target_dir.."/data" },
-        { "docs", ext.target_dir.."/docs" },
-        { "omni", ext.target_dir.."/omni" },
-    }
+repo_build.prebuild_link {
+    { "data", ext.target_dir.."/data" },
+    { "docs", ext.target_dir.."/docs" },
+    { "omni", ext.target_dir.."/omni" },
+}
