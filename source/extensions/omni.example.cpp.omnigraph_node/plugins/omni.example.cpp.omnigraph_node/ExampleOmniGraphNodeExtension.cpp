899fb740ef2000feccf6122e1291626e1aafea49
Kevin-Picott-NV <111305216+Kevin-Picott-NV@users.noreply.github.com>
2022-08-30 16:59:56 -0400

    Kpicott/om 26674/add omnigraph node example (#3)
    * OM-26674 Added definition of a simple OmniGraph node extension

--- source/extensions/omni.example.cpp.omnigraph_node/plugins/omni.example.cpp.omnigraph_node/ExampleOmniGraphNodeExtension.cpp
+++ source/extensions/omni.example.cpp.omnigraph_node/plugins/omni.example.cpp.omnigraph_node/ExampleOmniGraphNodeExtension.cpp
@@ -13,12 +13,20 @@
 
 #include <omni/ext/IExt.h>
 #include <omni/graph/core/IGraphRegistry.h>
+#include <omni/graph/core/ogn/Registration.h>
 
+// Standard plugin definitions required by Carbonite.
 const struct carb::PluginImplDesc pluginImplDesc = { "omni.example.cpp.omnigraph_node.plugin",
                                                      "An example C++ extension.", "NVIDIA",
                                                      carb::PluginHotReload::eEnabled, "dev" };
 
-CARB_PLUGIN_IMPL_DEPS(omni::graph::core::IGraphRegistry)
+// These interface dependencies are required by all OmniGraph node types
+CARB_PLUGIN_IMPL_DEPS(omni::graph::core::IGraphRegistry,
+                      carb::flatcache::IPath,
+                      carb::flatcache::IToken)
+
+// This macro sets up the information required to register your node type definitions with OmniGraph
+DECLARE_OGN_NODES()
 
 namespace omni
 {
@@ -35,11 +43,16 @@ public:
     void onStartup(const char* extId) override
     {
         printf("ExampleOmniGraphNodeExtension starting up (ext_id: %s).\n", extId);
+        // This macro walks the list of pending node type definitions and registers them with OmniGraph
+        INITIALIZE_OGN_NODES()
     }
 
     void onShutdown() override
     {
         printf("ExampleOmniGraphNodeExtension shutting down.\n");
+        // This macro walks the list of registered node type definitions and deregisters all of them. This is required
+        // for hot reload to work.
+        RELEASE_OGN_NODES()
     }
 
 private:
--- source/extensions/omni.example.cpp.omnigraph_node/premake5.lua
+++ source/extensions/omni.example.cpp.omnigraph_node/premake5.lua
@@ -1,16 +1,41 @@
--- Setup the extension.
+-- Setup the basic extension information.
 local ext = get_current_extension_info()
 project_ext(ext)
 
+
+-- --------------------------------------------------------------------------------------------------------------
+-- Helper variable containing standard configuration information for projects containing OGN files.
+local ogn = get_ogn_project_information(ext, "omni/example/cpp/omnigraph_node")
+
+
+-- --------------------------------------------------------------------------------------------------------------
 -- Link folders that should be packaged with the extension.
 repo_build.prebuild_link {
     { "data", ext.target_dir.."/data" },
     { "docs", ext.target_dir.."/docs" },
-    { "omni", ext.target_dir.."/omni" },
 }
 
+
+-- --------------------------------------------------------------------------------------------------------------
+-- Copy the __init__.py to allow building of a non-linked ogn/ import directory.
+-- In a mixed extension this would be part of a separate Python-based project but since here it is just the one
+-- file it can be copied directly with no build dependencies.
+repo_build.prebuild_copy {
+    { "omni/example/cpp/omnigraph_node/__init__.py", ogn.python_target_path }
+}
+
+
+-- --------------------------------------------------------------------------------------------------------------
+-- Breaking this out as a separate project ensures the .ogn files are processed before their results are needed.
+project_ext_ogn( ext, ogn )
+
+
+-- --------------------------------------------------------------------------------------------------------------
 -- Build the C++ plugin that will be loaded by the extension.
-project_ext_plugin(ext, "omni.example.cpp.omnigraph_node.plugin")
-    local plugin_name = "omni.example.cpp.omnigraph_node"
-    add_files("source", "plugins/"..plugin_name)
-    includedirs { "plugins/"..plugin_name }
+project_ext_plugin(ext, ogn.plugin_project)
+    -- It is important that you add all subdirectories containing C++ code to this project
+    add_files("source", "plugins/"..ogn.module)
+    add_files("nodes", "plugins/nodes")
+
+    -- Add the standard dependencies all OGN projects have; includes, libraries to link, and required compiler flags
+    add_ogn_dependencies(ogn)
