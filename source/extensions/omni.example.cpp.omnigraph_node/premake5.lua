de19908902afdc24ff0bc03b8a8f80d1a3d5d281
Kevin P Picott <kpicott@nvidia.com>
2022-08-30 16:57:18 -0400

    OM-26674 Added comments to the build script

--- source/extensions/omni.example.cpp.omnigraph_node/premake5.lua
+++ source/extensions/omni.example.cpp.omnigraph_node/premake5.lua
@@ -15,8 +15,11 @@ repo_build.prebuild_link {
     { "docs", ext.target_dir.."/docs" },
 }
 
+
 -- --------------------------------------------------------------------------------------------------------------
 -- Copy the __init__.py to allow building of a non-linked ogn/ import directory.
+-- In a mixed extension this would be part of a separate Python-based project but since here it is just the one
+-- file it can be copied directly with no build dependencies.
 repo_build.prebuild_copy {
     { "omni/example/cpp/omnigraph_node/__init__.py", ogn.python_target_path }
 }
@@ -30,9 +33,9 @@ project_ext_ogn( ext, ogn )
 -- --------------------------------------------------------------------------------------------------------------
 -- Build the C++ plugin that will be loaded by the extension.
 project_ext_plugin(ext, ogn.plugin_project)
+    -- It is important that you add all subdirectories containing C++ code to this project
     add_files("source", "plugins/"..ogn.module)
     add_files("nodes", "plugins/nodes")
-    includedirs { "plugins/"..ogn.module }
 
-    -- Add the standard dependencies all OGN projects have
+    -- Add the standard dependencies all OGN projects have; includes, libraries to link, and required compiler flags
     add_ogn_dependencies(ogn)
